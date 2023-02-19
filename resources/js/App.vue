d569013f5eaedad2c51138f6aa75005524a175be
fujioka <fujioka8700@gmail.com>
2023-02-19 22:53:56 +0900

    [update] v-class ディレクティブ 5。

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,11 +1,11 @@
 <script setup>
-const isActive = false;
+const isActive = true;
 const isBlack = true;
 </script>
 
 <template>
   <h1>Vue 3 入門</h1>
-  <p :class="isActive ? 'active' : 'underLine'">v-bindの設定方法の確認</p>
+  <p :class="isActive && 'active'">v-bindの設定方法の確認</p>
 </template>
 
 <style>
