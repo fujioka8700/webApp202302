2b54048c7ca7440b68506b28f4c491e8e057db5b
fujioka <fujioka8700@gmail.com>
2023-02-20 06:31:29 +0900

    [update] v-bind:style ディレクティブ 1

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,23 +1,11 @@
 <script setup>
-const isActive = "active";
-const isBlack = "back";
+const activeColor = "red";
+const fontSize = "3em";
 </script>
 
 <template>
   <h1>Vue 3 入門</h1>
-  <p :class="[isActive, isBlack]">v-bindの設定方法の確認</p>
+  <p :style="{ color: activeColor, fontSize: fontSize }">
+    v-bindの設定方法の確認
+  </p>
 </template>
-
-<style>
-.active {
-  color: red;
-}
-
-.underLine {
-  text-decoration: underline;
-}
-
-.back {
-  background-color: black;
-}
-</style>
