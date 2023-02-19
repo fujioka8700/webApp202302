fd76a89379140c48cb12bef5e02d1fed46d7463c
fujioka <fujioka8700@gmail.com>
2023-02-19 22:19:29 +0900

    [add] App.vue 追加しました。

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,3 +1,10 @@
+<script setup>
+const message = "<h2>Hello World</h2>";
+</script>
+
 <template>
-    <h1>Hello World</h1>
+  <h1>Vue 3 入門</h1>
+  <p>{{ message }}</p>
+  <p v-text="message"></p>
+  <div v-html="message"></div>
 </template>
