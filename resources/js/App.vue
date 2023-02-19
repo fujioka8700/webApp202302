bc4b7ec301e19a0351a8a8aaeb927155ca2144ea
fujioka <fujioka8700@gmail.com>
2023-02-19 22:33:05 +0900

    [update] v-bind ディレクティブ。

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,14 +1,11 @@
 <script setup>
-let message = "Hello World";
-
-const upperCase = () => {
-  message = message.toUpperCase();
-};
-
-upperCase();
+const link = "https://ja.vuejs.org/";
 </script>
 
 <template>
   <h1>Vue 3 入門</h1>
-  <div v-html="message"></div>
+  <div v-html="link"></div>
+  <div>
+    <a v-bind:href="link">Vue公式</a>
+  </div>
 </template>
