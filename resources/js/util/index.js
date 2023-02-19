de904fbe2e0180fe6ecf66949569431c6a1ccff3
fujioka <fujioka8700@gmail.com>
2023-02-19 17:10:12 +0900

    first commit

---
+++ resources/js/util/index.js
@@ -0,0 +1,21 @@
+/**
+ * クッキーの値を取得します。
+ * @param {String} searchKey 検索するキーです。
+ * @returns {String} キーに対応する値です。
+ */
+export function getCookieValue(searchKey) {
+    if (typeof searchKey === "undefined") {
+        return "";
+    }
+
+    let val = "";
+
+    document.cookie.split(";").forEach((cookie) => {
+        const [key, value] = cookie.split("=");
+        if (key === searchKey) {
+            return (val = value);
+        }
+    });
+
+    return val;
+}
