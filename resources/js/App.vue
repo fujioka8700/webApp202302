cc06de4d80106c41ac60362dce6eb83913c36837
fujioka <fujioka8700@gmail.com>
2023-02-21 23:04:44 +0900

    [update] v-for ディレクティブ 2

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,15 +1,16 @@
 <script setup>
-const languages = ["JavaScript", "TypeScript", "Vue.js"];
+const users = [
+  { id: 1, name: "John Doe", email: "john@test.com", admin: true },
+  { id: 2, name: "Jane Doe", email: "jane@example.com", admin: false },
+  { id: 3, name: "Kevin MacDonald", email: "kevin@foo.com", admin: false },
+];
 </script>
 
 <template>
   <h1>Vue 3 入門</h1>
   <ul>
-    <li>{{ languages[0] }}</li>
-    <li>{{ languages[1] }}</li>
-    <li>{{ languages[2] }}</li>
-  </ul>
-  <ul>
-    <li v-for="language in languages" :key="language">{{ language }}</li>
+    <li v-for="user in users" :key="user.id">
+      {{ user.id }}:{{ user.name }}({{ user.email }})
+    </li>
   </ul>
 </template>
