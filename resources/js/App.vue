d8cadeb2d7cc3dc3a93beceb35bf7814d9cbfbe0
fujioka <fujioka8700@gmail.com>
2023-02-21 22:02:54 +0900

    [update] v-if と v-show の違い 1

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,8 +1,9 @@
 <script setup>
-const error = true;
+const error = false;
 </script>
 
 <template>
   <h1>Vue 3 入門</h1>
+  <div v-if="error">エラーが発生しています。</div>
   <div v-show="error">エラーが発生しています。</div>
 </template>
