3b268578017e0f3c42ad0d8eaba5388da57edb1b
fujioka <fujioka8700@gmail.com>
2023-02-21 22:01:18 +0900

    [update] v-show ディレクティブ 1

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,11 +1,8 @@
 <script setup>
-const stock = 0;
+const error = true;
 </script>
 
 <template>
   <h1>Vue 3 入門</h1>
-  <div>商品数:{{ stock }}</div>
-  <div v-if="stock > 5">まだ商品の在庫数に余裕があります。</div>
-  <div v-else-if="stock === 0">申し訳ございません。現在売り切れです。</div>
-  <div v-else>在庫数が少なくなってきます。お急ぎください。</div>
+  <div v-show="error">エラーが発生しています。</div>
 </template>
