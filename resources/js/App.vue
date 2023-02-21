5ba0c02c22cc4fe9cb4590e4ebe81e66edc87716
fujioka <fujioka8700@gmail.com>
2023-02-21 21:56:36 +0900

    [update] v-if ディレクティブ 4

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,9 +1,11 @@
 <script setup>
-const error = "Error 404";
+const stock = 0;
 </script>
 
 <template>
   <h1>Vue 3 入門</h1>
-  <div v-if="error">{{ error }}</div>
-  <div v-else>エラーはありません。</div>
+  <div>商品数:{{ stock }}</div>
+  <div v-if="stock > 5">まだ商品の在庫数に余裕があります。</div>
+  <div v-else-if="stock === 0">申し訳ございません。現在売り切れです。</div>
+  <div v-else>在庫数が少なくなってきます。お急ぎください。</div>
 </template>
