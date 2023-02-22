b1cf92674f893c7fe5af3c39e284d93c6b0daa1f
fujioka <fujioka8700@gmail.com>
2023-02-22 21:39:05 +0900

    [update] clickイベント

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,31 +1,14 @@
-<script setup>
-const users = [
-  {
-    id: 1,
-    name: "John Doe",
-    email: "john@test.com",
-    admin: true,
-  },
-  {
-    id: 2,
-    name: "Nakamura Toru",
-    email: "nakamura@toru.com",
-    admin: false,
-  },
-  {
-    id: 3,
-    name: "Shimada Ryo",
-    email: "shimada@ryo.com",
-    admin: false,
-  },
-];
-</script>
-
 <template>
-  <h1>リストと分岐</h1>
-  <div v-for="user in users" :key="user.id">
-    <div v-if="!user.admin">
-      {{ user.name }}
-    </div>
+  <div>
+    <h1>clickイベント</h1>
+    <button v-on:click="clickButton">クリック</button>
   </div>
 </template>
+
+<script setup>
+const clickButton = () => {
+  console.log("val");
+};
+</script>
+
+<style lang="scss" scoped></style>
