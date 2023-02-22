95548a8748e3e09a9959c7a2398b17f2c609535a
fujioka <fujioka8700@gmail.com>
2023-02-22 21:26:28 +0900

    [update] オブジェクトの値をリスト化

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,15 +1,31 @@
 <script setup>
-const user = {
-  id: 1,
-  name: "John Doe",
-  email: "john@test.com",
-  admin: true,
-};
+const users = [
+  {
+    id: 1,
+    name: "John Doe",
+    email: "john@test.com",
+    admin: true,
+  },
+  {
+    id: 2,
+    name: "Nakamura Toru",
+    email: "nakamura@toru.com",
+    admin: false,
+  },
+  {
+    id: 3,
+    name: "Shimada Ryo",
+    email: "shimada@ryo.com",
+    admin: false,
+  },
+];
 </script>
 
 <template>
-  <h1>オブジェクトのリスト化2</h1>
+  <h1>オブジェクトの値をリスト化</h1>
   <ul>
-    <li v-for="(value, name) in user" :key="value">{{ name }}:{{ value }}</li>
+    <li v-for="user in users" :key="user.id">
+      <div v-for="(value, name) in user">{{ name }}:{{ value }}</div>
+    </li>
   </ul>
 </template>
