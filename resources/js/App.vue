8655b3a6ba0c618855e5b3501a199b6010a85b44
fujioka <fujioka8700@gmail.com>
2023-02-22 20:53:58 +0900

    [update] オブジェクトのリスト化 1

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,53 +1,15 @@
 <script setup>
-const users = [
-  { id: 1, name: "John Doe", email: "john@test.com", admin: true },
-  { id: 2, name: "Jane Doe", email: "jane@example.com", admin: false },
-  { id: 3, name: "Kevin MacDonald", email: "kevin@foo.com", admin: false },
-];
+const user = {
+  id: 1,
+  name: "John Doe",
+  email: "john@test.com",
+  admin: true,
+};
 </script>
 
 <template>
-  <h1>Vue 3 入門</h1>
+  <h1>オブジェクトのリスト化</h1>
   <ul>
-    <li v-for="user in users" :key="user.id">
-      {{ user.id }}:{{ user.name }}({{ user.email }})
-    </li>
+    <li v-for="value in user" :key="value">{{ value }}</li>
   </ul>
-
-  <ul>
-    <li v-for="(user, index) in users" :key="user">
-      {{ index }}-{{ user.id }}:{{ user.name }}({{ user.email }})
-    </li>
-  </ul>
-
-  <template v-for="user of users" :key="user.id">
-    {{ user.id }}{{ user.name }}{{ user.email }}
-  </template>
-
-  <table>
-    <thead>
-      <tr>
-        <td>ID</td>
-        <td>ユーザー名</td>
-        <td>Email</td>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="user in users" :key="user.id">
-        <td>{{ user.id }}</td>
-        <td>{{ user.name }}</td>
-        <td>{{ user.email }}</td>
-      </tr>
-    </tbody>
-  </table>
 </template>
-
-<style scoped>
-table {
-  border-collapse: collapse;
-}
-table th,
-table td {
-  border: solid 1px black;
-}
-</style>
