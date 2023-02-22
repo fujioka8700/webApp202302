7365d77d4f2b2c84cb202ea5f685d1b1f30c22b4
fujioka <fujioka8700@gmail.com>
2023-02-23 08:35:45 +0900

    [update] clickイベント 5

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
     <h1>clickイベント</h1>
-    <button @click="clickButton">クリック</button>
-    <div class="square" @click="clickButton('クリックされました')"></div>
+    <button v-on:click="clickButton('クリック'), another('click')">
+      クリック
+    </button>
   </div>
 </template>
 
@@ -10,12 +11,9 @@
 const clickButton = (msg) => {
   console.log(msg);
 };
+const another = (msg) => {
+  console.log(msg);
+};
 </script>
 
-<style lang="scss" scoped>
-.square {
-  width: 100px;
-  height: 100px;
-  background-color: #39a9d6;
-}
-</style>
+<style lang="scss" scoped></style>
