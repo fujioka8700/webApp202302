07cb852a0e51f78f2ba88b20e202edf24efe2d21
fujioka <fujioka8700@gmail.com>
2023-02-23 14:16:46 +0900

    [update] Watcher 1

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,32 +1,17 @@
 <template>
   <div>
-    <h1>Computed セッター</h1>
-    <h2>fullName: {{ fullName }}</h2>
-    <button @click="changeName">Change Name</button>
+    <h1>Watcher</h1>
+    <button @click="count++">Count:{{ count }}</button>
   </div>
 </template>
 
 <script setup>
-import { reactive, computed } from "vue";
-const user = reactive({
-  firstName: "John",
-  lastName: "Doe",
-});
+import { ref, watch } from "vue";
+const count = ref(0);
 
-const fullName = computed({
-  get() {
-    return `${user.firstName} ${user.lastName}`;
-  },
-  set(newValue) {
-    const names = newValue.split(" ");
-    user.firstName = names[0];
-    user.lastName = names[names.length - 1];
-  },
+watch(count, (count) => {
+  console.log("count:", count);
 });
-
-const changeName = () => {
-  fullName.value = "Jane Doe";
-};
 </script>
 
 <style lang="scss" scoped></style>
