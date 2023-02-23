0e8531b6b2eaf06f8bc72d4341839e347c453d36
fujioka <fujioka8700@gmail.com>
2023-02-23 22:03:55 +0900

    [update] Vue3 2週目。 9

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,13 +1,17 @@
 <template>
   <div>
     <h1>Reactivity</h1>
-    <button @click="count++">coint is:{{ count }}</button>
+    <button @click="addCount">coint is:{{ count }}</button>
   </div>
 </template>
 
 <script setup>
 import { ref } from "vue";
 const count = ref(0);
+
+const addCount = () => {
+  count.value++;
+};
 </script>
 
 <style lang="scss" scoped></style>
