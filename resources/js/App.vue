51d88ae0fdadab63b7a6a0d8b392478da8e00bba
fujioka <fujioka8700@gmail.com>
2023-02-23 13:21:34 +0900

    [update] Computed vs Function 2

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,20 +1,20 @@
 <template>
   <div>
     <h1>Computed vs Function</h1>
-    <h2>fullName: {{ fullName() }}</h2>
+    <h2>fullName: {{ fullName }}</h2>
   </div>
 </template>
 
 <script setup>
-import { reactive } from "vue";
+import { reactive, computed } from "vue";
 const user = reactive({
   firstName: "John",
   lastName: "Doe",
 });
 
-const fullName = () => {
+const fullName = computed(() => {
   return `${user.firstName} ${user.lastName}`;
-};
+});
 </script>
 
 <style lang="scss" scoped></style>
