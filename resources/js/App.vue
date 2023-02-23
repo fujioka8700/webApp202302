34db21e9d98fc07d7245805b7d13bc197e54e55e
fujioka <fujioka8700@gmail.com>
2023-02-23 13:28:54 +0900

    [update] Computed vs Function 3

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
     <h1>Computed vs Function</h1>
-    <h2>fullName: {{ fullName }}</h2>
+    <h2>fullName: {{ fullName() }}</h2>
+    <h2>fullName: {{ fullName() }}</h2>
+    <h2>fullName: {{ fullName() }}</h2>
+    <h2>fullName: {{ cFullName }}</h2>
+    <h2>fullName: {{ cFullName }}</h2>
+    <h2>fullName: {{ cFullName }}</h2>
   </div>
 </template>
 
@@ -12,9 +17,12 @@ const user = reactive({
   lastName: "Doe",
 });
 
-const fullName = computed(() => {
-  return `${user.firstName} ${user.lastName}`;
-});
+const fullName = () =>
+  `${Math.floor(Math.random() * 100)} ${user.firstName} ${user.lastName}`;
+
+const cFullName = computed(
+  () => `${Math.floor(Math.random() * 100)} ${user.firstName} ${user.lastName}`
+);
 </script>
 
 <style lang="scss" scoped></style>
