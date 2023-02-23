9228c339d2518fd04c9a587d62af0be0fd6b5d4e
fujioka <fujioka8700@gmail.com>
2023-02-23 22:47:50 +0900

    [update] Vue3 2週目。 15

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,23 +1,32 @@
 <template>
   <div>
-    <h1>Computed 2</h1>
-    <ul>
-      <li v-for="user in adminUsers" :key="user.id">
-        {{ user.id }} {{ user.name }} {{ user.email }}
-      </li>
-    </ul>
+    <h1>Computed セッター</h1>
+    <h2>{{ fullName }}</h2>
+    <button @click="changeName">Change Name</button>
   </div>
 </template>
 
 <script setup>
-import { computed } from "vue";
-const users = [
-  { id: 1, name: "John Doe", email: "john@test.com", admin: true },
-  { id: 2, name: "Jane Doe", email: "jane@example.com", admin: false },
-  { id: 3, name: "Kevin MacDonald", email: "kevin@test.com", admin: false },
-];
+import { reactive, computed } from "vue";
+const user = reactive({
+  firstName: "John",
+  lastName: "Doe",
+});
 
-const adminUsers = computed(() => users.filter((user) => user.admin === true));
+const fullName = computed({
+  get() {
+    return user.firstName + " " + user.lastName;
+  },
+  set(newValue) {
+    const names = newValue.split(" ");
+    user.firstName = names[0];
+    user.lastName = names[names.length - 1];
+  },
+});
+
+const changeName = () => {
+  fullName.value = "Jane Doe";
+};
 </script>
 
 <style lang="scss" scoped></style>
