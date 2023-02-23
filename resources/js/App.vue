15a5edaba28f7f5f20916caa0fd1a40cb065f017
fujioka <fujioka8700@gmail.com>
2023-02-23 20:59:34 +0900

    [update] Vue3 2週目。

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,20 +1,7 @@
 <template>
-  <div>
-    <h1>watchEffect</h1>
-    <button @click="count++">Count:{{ count }}</button>
-    <button @click="count2++">Count2:{{ count2 }}</button>
-  </div>
+  <div></div>
 </template>
 
-<script setup>
-import { ref, watchEffect } from "vue";
-
-const count = ref(0);
-const count2 = ref(100);
-
-watchEffect(() => {
-  console.log(parseInt(count.value, 10) / parseInt(count2.value, 10));
-});
-</script>
+<script setup></script>
 
 <style lang="scss" scoped></style>
