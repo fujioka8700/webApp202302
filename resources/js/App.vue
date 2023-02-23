282c1e62b6b16ce1d3c6c2e376a54a993564388f
fujioka <fujioka8700@gmail.com>
2023-02-23 11:51:58 +0900

    [update] Computed vs Function 1

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,25 +1,20 @@
 <template>
   <div>
-    <h1>computed ディレクティブ</h1>
-    <div v-for="user in adminUsers" :key="user.id">
-      <p>{{ user.id }} {{ user.name }} {{ user.email }}</p>
-    </div>
+    <h1>Computed vs Function</h1>
+    <h2>fullName: {{ fullName() }}</h2>
   </div>
 </template>
 
 <script setup>
-import { computed } from "vue";
-const users = [
-  { id: 1, name: "John Doe", email: "john@test.com", admin: true },
-  { id: 2, name: "Jane Doe", email: "jane@example.com", admin: false },
-  { id: 3, name: "Kevin MacDonald", email: "kevin@test.com", admin: false },
-];
-
-const adminUsers = computed(() => {
-  return users.filter((user) => {
-    return user.admin === true;
-  });
+import { reactive } from "vue";
+const user = reactive({
+  firstName: "John",
+  lastName: "Doe",
 });
+
+const fullName = () => {
+  return `${user.firstName} ${user.lastName}`;
+};
 </script>
 
 <style lang="scss" scoped></style>
