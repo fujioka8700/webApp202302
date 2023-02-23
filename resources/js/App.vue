b7fbdbedf8529a161007cf409c04cc0ee50f14ce
fujioka <fujioka8700@gmail.com>
2023-02-23 10:00:19 +0900

    [update] reactive の設定 2

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>reactiveの設定</h1>
-    <button @click="state.count++">count is: {{ state.count }}</button>
+    <button @click="addCount">count is: {{ state.count }}</button>
   </div>
 </template>
 
@@ -10,6 +10,10 @@ import { reactive } from "vue";
 const state = reactive({
   count: 0,
 });
+
+const addCount = () => {
+  state.count++;
+};
 </script>
 
 <style lang="scss" scoped></style>
