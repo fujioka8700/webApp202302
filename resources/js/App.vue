83b268b70593aa7bc77075e4d250d6ad368178b2
fujioka <fujioka8700@gmail.com>
2023-02-23 09:46:09 +0900

    [update] Reactivity(反応性) 2

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
     <h1>Reactivity(反応性)</h1>
-    <button @click="count++">count is:{{ count }}</button>
+    <button @click="addCount">count is:{{ count }}</button>
   </div>
 </template>
 
 <script setup>
-const count = 0;
+import { ref } from "vue";
+const count = ref(0);
+
+const addCount = () => {
+  count.value++;
+};
 </script>
 
 <style lang="scss" scoped></style>
