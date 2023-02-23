ae79d522760cb6dc0cecc450546ed3ea553c5762
fujioka <fujioka8700@gmail.com>
2023-02-23 21:22:22 +0900

    [update] Vue3 2週目。 2

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,7 +1,46 @@
 <template>
-  <div></div>
+  <div>
+    <h1>Vue3 入門</h1>
+    <p class="underLine" :class="{ active: isActive }">{{ message }}</p>
+    <p :style="{ color: activeColor }">
+      {{ message.length > 10 ? "Long" : "Short" }}
+    </p>
+    <p :style="styleObject" v-text="message"></p>
+    <p v-html="message2"></p>
+    <a :href="link" target="_blank">JavaScriipt リファレンス</a>
+    <div v-if="error">{{ error }}</div>
+    <div v-else>異常なし</div>
+    <div v-show="error">エラーが発生しました。</div>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+let message = "Hello World";
+const message2 = "<h2>Hello World</h2>";
+const link = "https://developer.mozilla.org/ja/";
+const isActive = true;
+const activeColor = "red";
+const styleObject = {
+  color: "blue",
+  fontWeight: 900,
+};
 
-<style lang="scss" scoped></style>
+const error = "エラー発生";
+
+const lowerCase = () => {
+  message = message.toLowerCase();
+};
+
+lowerCase();
+</script>
+
+<style lang="scss" scoped>
+.active {
+  color: red;
+  font-size: 2em;
+}
+
+.underLine {
+  text-decoration: underline;
+}
+</style>
