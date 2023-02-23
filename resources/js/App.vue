aed5b8259a654a852d0248f53a7f3709992a7e2a
fujioka <fujioka8700@gmail.com>
2023-02-23 22:29:32 +0900

    [update] Vue3 2週目。 14

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,18 +1,23 @@
 <template>
   <div>
-    <h1>Computed</h1>
-    <h2>fullName: {{ fullName }}</h2>
+    <h1>Computed 2</h1>
+    <ul>
+      <li v-for="user in adminUsers" :key="user.id">
+        {{ user.id }} {{ user.name }} {{ user.email }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script setup>
-import { reactive, computed } from "vue";
-const user = reactive({
-  firstName: "John",
-  lastName: "Doe",
-});
+import { computed } from "vue";
+const users = [
+  { id: 1, name: "John Doe", email: "john@test.com", admin: true },
+  { id: 2, name: "Jane Doe", email: "jane@example.com", admin: false },
+  { id: 3, name: "Kevin MacDonald", email: "kevin@test.com", admin: false },
+];
 
-const fullName = computed(() => `${user.firstName} ${user.lastName}`);
+const adminUsers = computed(() => users.filter((user) => user.admin === true));
 </script>
 
 <style lang="scss" scoped></style>
