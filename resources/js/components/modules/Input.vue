037d109118a0db3bd80f600c7c07ef0e4ec5432b
fujioka <fujioka8700@gmail.com>
2023-02-24 14:04:55 +0900

    [update] Input 要素をコンポーネント化 2

---
+++ resources/js/components/modules/Input.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <input
+      type="text"
+      :value="props.modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+    />
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+
+const props = defineProps({
+  modelValue: String,
+});
+</script>
+
+<style lang="scss" scoped></style>
