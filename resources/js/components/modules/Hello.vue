93862995c4ce08a29688edc07c1ed2fbd2120a5d
fujioka <fujioka8700@gmail.com>
2023-02-24 11:37:04 +0900

    [update] emit によるイベント発生と検知 1

--- resources/js/components/modules/Hello.vue
+++ resources/js/components/modules/Hello.vue
@@ -1,13 +1,17 @@
 <template>
-  <!-- <div class="m-hello"> -->
-  <h2>Hello コンポーネント</h2>
-  <button @click="$emit('notification')">通知</button>
-  <!-- </div> -->
+  <div class="m-hello">
+    <h2>Hello コンポーネント</h2>
+    <button @click="sendNotification">通知</button>
+  </div>
 </template>
 
 <script setup>
 import { defineEmits } from "vue";
 const emit = defineEmits(["notification"]);
+
+const sendNotification = () => {
+  emit("notification");
+};
 </script>
 
 <style lang="scss" scoped>
