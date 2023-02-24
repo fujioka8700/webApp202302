113303708e500a330d10d56105db12b990e69d4e
fujioka <fujioka8700@gmail.com>
2023-02-24 09:40:38 +0900

    [update] Vue3 初めてのコンポーネント 1

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,17 +1,14 @@
 <template>
   <div>
-    <h1>watchEffect</h1>
-    <button @click="count1++">Count1:{{ count1 }}</button>
-    <button @click="count2++">Count2:{{ count2 }}</button>
+    <h1>Vue3 初めてのコンポーネント</h1>
+    <Hello />
+    <Hello />
+    <Hello />
   </div>
 </template>
 
 <script setup>
-import { ref, watchEffect } from "vue";
-const count1 = ref(0);
-const count2 = ref(100);
-
-watchEffect(() => console.log(count1.value));
+import Hello from "./components/modules/Hello.vue";
 </script>
 
 <style lang="scss" scoped></style>
