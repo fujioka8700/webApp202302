7bbba2a498ecb698f7b0e2c6680c4b2b09013992
fujioka <fujioka8700@gmail.com>
2023-02-24 09:48:24 +0900

    [update] ref, reactive 関数の利用 1

---
+++ resources/js/components/modules/Hello.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h2>初めてのコンポーネント</h2>
+    <p>Ref Count: {{ count }}</p>
+    <p>Reactive Count: {{ state.count }}</p>
+    <div>
+      <button @click="addRefCount">Ref Count+</button>
+      <button @click="addReactiveCount">Reactive Count+</button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, reactive } from "vue";
+const count = ref(0);
+const state = reactive({
+  count: 1,
+});
+
+const addRefCount = () => {
+  count.value++;
+};
+
+const addReactiveCount = () => {
+  state.count++;
+};
+</script>
+
+<style lang="scss" scoped></style>
