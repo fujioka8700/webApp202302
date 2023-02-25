836e434edf42e5b755e58e090328d718e935ce7a
fujioka <fujioka8700@gmail.com>
2023-02-25 09:42:18 +0900

    [update] Slot 1

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,26 +1,11 @@
 <template>
   <div class="app">
-    <h1>バリデーションの設定</h1>
-    <p>{{ name }}</p>
-    <Hello :name="name" @changeNameEvent="handleEvent" />
-    <p>{{ name }}</p>
-    <p>{{ address }}</p>
-    <Input v-model="name" />
-    <Input v-model="address" />
+    <User>服部 英行</User>
   </div>
 </template>
 
 <script setup>
-import Input from "./components/modules/Input.vue";
-import Hello from "./components/modules/Hello.vue";
-import { ref } from "vue";
-
-const name = ref("John");
-const address = ref("");
-
-const handleEvent = (newName) => {
-  name.value = newName;
-};
+import User from "./components/modules/User.vue";
 </script>
 
 <style lang="scss" scoped>
