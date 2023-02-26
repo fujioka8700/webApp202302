9ad8e3a04bdb87cbd16cc746f3e24a7d73f7cb38
fujioka <fujioka8700@gmail.com>
2023-02-26 19:15:27 +0900

    [update] emit でデータを渡す 4

--- resources/js/components/modules/TheHello.vue
+++ resources/js/components/modules/TheHello.vue
@@ -3,7 +3,12 @@
     <h2>Hello コンポーネント</h2>
     <p>名前：{{ props.name }}</p>
     <button @click="sendNotification">通知</button>
-    <input type="text" v-model="name" @input="changeName" />
+    <!-- <input type="text" v-model="name" @input="changeName" /> -->
+    <input
+      type="text"
+      :value="props.name"
+      @input="$emit('changeNameEvent', $event.target.value)"
+    />
   </div>
 </template>
 
@@ -13,13 +18,13 @@ const props = defineProps({
   name: String,
 });
 
-const name = ref(props.name);
+// const name = ref(props.name);
 
-const emit = defineEmits(["changeNameEvent"]);
+// const emit = defineEmits(["changeNameEvent"]);
 
-const changeName = () => {
-  emit("changeNameEvent", name.value);
-};
+// const changeName = () => {
+//   emit("changeNameEvent", name.value);
+// };
 </script>
 
 <style lang="scss" scoped>
