c7b37f1f901b7c1668d51336b818da2fb558f06e
fujioka <fujioka8700@gmail.com>
2023-02-26 09:29:21 +0900

    [update] Dynamic コンポーネント 2

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -9,15 +9,25 @@
       <Tokyo v-if="city == 'tokyo'" />
       <Kyoto v-else />
     </div>
+    <div>
+      <component v-bind:is="tab"></component>
+    </div>
   </div>
 </template>
 
 <script setup>
 import Tokyo from "./components/modules/Tokyo.vue";
 import Kyoto from "./components/modules/Kyoto.vue";
-import { ref } from "vue";
+import { ref, computed } from "vue";
 
 const city = ref("tokyo");
+
+const tabs = {
+  tokyo: "Tokyo",
+  kyoto: "Kyoto",
+};
+
+const tab = computed(() => tabs[city.value]);
 </script>
 
 <style lang="scss" scoped>
