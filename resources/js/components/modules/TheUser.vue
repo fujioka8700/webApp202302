1e6d38eecbe4ed64b7f33ec7b077d3565deb3924
fujioka <fujioka8700@gmail.com>
2023-02-26 21:58:32 +0900

    [update] Slot 2

---
+++ resources/js/components/modules/TheUser.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="m-theuser">
+    <p>くにおくんが在籍している学校:<slot></slot></p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.m-theuser {
+  background: #f5f5dc;
+}
+</style>
