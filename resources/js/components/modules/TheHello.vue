6cc01ecb483687dffa7c4a4be3c8f90a4ada9691
fujioka <fujioka8700@gmail.com>
2023-02-26 14:39:56 +0900

    [update] Vue3 Component 2周目 12

--- resources/js/components/modules/TheHello.vue
+++ resources/js/components/modules/TheHello.vue
@@ -1,11 +1,13 @@
 <template>
-  <div class="m-hello">
-    <h2>Hello コンポーネント</h2>
-    <button @click="$emit('notification')">通知</button>
-  </div>
+  <!-- <div class="m-hello"> -->
+  <h2>Hello コンポーネント</h2>
+  <button @click="$emit('notification')">通知</button>
+  <!-- </div> -->
 </template>
 
-<script setup></script>
+<script setup>
+const emits = defineEmits(["notification"]);
+</script>
 
 <style lang="scss" scoped>
 .m-hello {
