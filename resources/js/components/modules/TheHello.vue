998dc865a02f71a75af19ab0dcbb3b9a45aa1b74
fujioka <fujioka8700@gmail.com>
2023-02-26 13:33:48 +0900

    [update] Vue3 Component 2周目 2

---
+++ resources/js/components/modules/TheHello.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="m-hello">
+    <h2>初めてのコンポーネント</h2>
+    <p>Count:{{ count }}</p>
+    <p>Reactive Count: {{ state.count }}</p>
+    <div>
+      <button @click="addRefCount">Ref Count++</button>
+      <button @click="addReactiveCount">Reactive Count++</button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, reactive } from "vue";
+
+const count = ref(0);
+const state = reactive({
+  count: 1,
+});
+
+const addRefCount = () => {
+  count.value++;
+};
+
+const addReactiveCount = () => {
+  state.count++;
+};
+</script>
+
+<style lang="scss" scoped>
+.m-hello {
+  background: #fff0f5;
+}
+</style>
