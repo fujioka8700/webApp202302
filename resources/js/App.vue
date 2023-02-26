34d1a273f36cc8ac8b2e86998b15ed53620ee6d2
fujioka <fujioka8700@gmail.com>
2023-02-26 10:44:33 +0900

    [update] Provide / Inject Reactiveな変数を渡す 4

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
-    <h1>Provide / Inject</h1>
+    <h1>Provide / Inject Reactiveな変数を渡す</h1>
     <CompA message="propsでデータ渡し" />
+    <input type="text" v-model="message" />
   </div>
 </template>
 
 <script setup>
 import CompA from "./components/modules/CompA.vue";
-import { provide } from "vue";
+import { ref, provide } from "vue";
 
-provide("message", "Provide/Injectでデータ渡し");
+const message = ref("Provide/Injectでデータ渡し");
+
+provide("message", message);
 </script>
 
 <style lang="scss" scoped></style>
