20be1cf4058d4eef64d0188aa6414cee43a4fba9
fujioka <fujioka8700@gmail.com>
2023-03-04 12:19:06 +0900

    [update] $patchメソッド 1

--- resources/js/components/modules/HelloPinia.vue
+++ resources/js/components/modules/HelloPinia.vue
@@ -3,12 +3,22 @@
     <h3>Hello Pinia</h3>
     <p>カウント:{{ counter.count }}</p>
     <button @click="counter.$reset">リセット</button>
+    <button @click="patch">Patch</button>
   </div>
 </template>
 
 <script setup>
 import { useStoreCounter } from "../../store/counter";
 const counter = useStoreCounter();
+
+const patch = () => {
+  counter.$patch({
+    count: 100,
+    user: {
+      name: "Jane Doe",
+    },
+  });
+};
 </script>
 
 <style lang="scss" scoped>
