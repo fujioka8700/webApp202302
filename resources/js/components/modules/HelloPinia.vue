6603c019a57a0f8621d76c834fadb698501040af
fujioka <fujioka8700@gmail.com>
2023-03-04 12:25:49 +0900

    [update] $replace 1

--- resources/js/components/modules/HelloPinia.vue
+++ resources/js/components/modules/HelloPinia.vue
@@ -5,6 +5,7 @@
     <p>ユーザー:{{ counter.user.name }}</p>
     <button @click="counter.$reset">リセット</button>
     <button @click="patch">Patch</button>
+    <button @click="replace">Replace</button>
   </div>
 </template>
 
@@ -17,6 +18,16 @@ const patch = () => {
     state.count = 10;
     state.user.name = "Kevin Doe";
   });
+};
+
+const replace = () => {
+  counter.$state = {
+    count: 500,
+    count2: 800,
+    user: {
+      name: "Jane Doe",
+    },
+  };
 };
 </script>
 
