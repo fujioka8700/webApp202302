57f83e4d3d9bc2a6b5a35ff0141659f3a4e3e115
fujioka <fujioka8700@gmail.com>
2023-03-04 10:03:05 +0900

    [update] リダイレクト 2

---
+++ resources/js/components/pages/NotFound.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h2>NotFound</h2>
+    <p>アクセスしたページは存在しません</p>
+    <button @click="$router.push({ name: 'home' })">Homeへ戻る</button>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
